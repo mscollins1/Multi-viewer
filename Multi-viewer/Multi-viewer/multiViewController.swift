c4219b07d1fb4ba0ad5e248eda37a195714e7b7b
mike <mikescollins@Mikes-MBP.fios-router.home>
2018-12-01 01:14:03 -0500

    addview button works

--- Multi-viewer/Multi-viewer/multiViewController.swift
+++ Multi-viewer/Multi-viewer/multiViewController.swift
@@ -21,19 +21,51 @@ class multiViewController: UIViewController {
     
     @IBOutlet weak var toolbar: UIToolbar!
     
+    
+    @IBAction func removeWebview(_ sender: Any) {
+        //can either do this as an alert or a pop over (lets try popover)
+        
+    }
+    
+    @IBAction func addWebview(_ sender: Any) {
+        //check to see if we should even add
+        if webviewArray.count == 3{
+            return
+        }
+        
+        //create new webview object
+        webviewArray.append(WKWebView())
+        //adjust numscreens to account for new # of views
+        numScreens += 1
+        
+        //will need to check for orientation and then update frames
+        if UIDevice.current.orientation.isLandscape {
+            landscapeOrientation()
+        } else {
+            portraitOrientation()
+        }
+        //load google as default search engine
+        //let newURL = URL(string: URLs[i]!)
+        //let newRequest = URLRequest(url: newURL!)
+        webviewArray[2].load(URLRequest(url: URL(string: defaultURL)!))
+        //finally add the latest webview as a subview
+        self.view.addSubview(webviewArray[2])
+        //keep toolbar in front
+        self.view.bringSubviewToFront(toolbar)
+    }
+    
+    
+    
+    
     @IBAction func returnToConfig(_ sender: Any) {
         for i in 0...(presentingViewController as! configViewController).urlArray.count-1{
             (presentingViewController as! configViewController).urlArray[i].text = URLs[i]
         }
-        //(presentingViewController as! configViewController).url1.text = URLs[0]
-        //(presentingViewController as! configViewController).url2.text = URLs[1]
-        //(presentingViewController as! configViewController).url3.text = URLs[2]
-
         self.dismiss(animated: true, completion: nil)
     }
     
     
-    func setupScreens(){
+    func initialSetupScreens(){
         let maxWidth = UIScreen.main.bounds.width
         let maxHeight = UIScreen.main.bounds.height/CGFloat(numScreens)
         
@@ -90,7 +122,7 @@ class multiViewController: UIViewController {
         } else {
             numScreens = 3
         }
-        setupScreens()
+        initialSetupScreens()
         view.backgroundColor = UIColor.cyan
         self.view.bringSubviewToFront(toolbar)
     }
