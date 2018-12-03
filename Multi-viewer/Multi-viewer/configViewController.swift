fd382a017f47b8ac9d64597ce5c93d2897d5ac6b
mike <mikescollins@Mikes-MBP.fios-router.home>
2018-12-03 13:53:47 -0500

    added 2 functions to hide keyboard when done editing (enterKeyboard and touchesBegan)

--- Multi-viewer/Multi-viewer/configViewController.swift
+++ Multi-viewer/Multi-viewer/configViewController.swift
@@ -94,6 +94,17 @@ class configViewController: UIViewController {
     }
     
     
+    @IBAction func keyboardEnter(_ sender: UITextField) {
+        //dismisses keyboard after hitting enter
+        sender.resignFirstResponder()
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
